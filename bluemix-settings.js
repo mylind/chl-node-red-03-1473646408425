a3b938c5ee9e806e1e1d4a11395fa3fee2c97d4f
Hong Liang Chen <chenhl@cn.ibm.com>
2016-09-12 17:12:45 +0800

    Enable security

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -45,7 +45,7 @@ var settings = module.exports = {
 
     // You can protect the user interface with a userid and password by using the following property
     // the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
-    //httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
+    httpAdminAuth: {user:"admin",pass:"$2a$08$/JsrRGlh372gVBMIs2TnYeY/.nJ7n5C2BUNBTSKIYcqrEJehkNv9."},
 
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
